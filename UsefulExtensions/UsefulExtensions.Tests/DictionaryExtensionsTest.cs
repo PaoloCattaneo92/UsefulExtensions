1fe13482f686068174a9989b5d6134846cbac56e
Paolo Cattaneo <paolo.cattaneo92@gmail.com>
2023-07-20 10:55:55 +0200

    Added DictionaryExtensions

---
+++ UsefulExtensions/UsefulExtensions.Tests/DictionaryExtensionsTest.cs
@@ -0,0 +1,56 @@
+﻿using PaoloCattaneo.UsefulExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Faker;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+namespace UsefulExtensions.Tests;
+
+internal class DictionaryExtensionsTest
+{
+    private Dictionary<string, int> dictionary;
+
+    [SetUp]
+    public void Setup()
+    {
+        dictionary = new Dictionary<string, int>()
+        {
+            { "Paolo", 31 },
+            { "Clara", 23 },
+            { "Francesco", 60 }
+        };
+    }
+
+    [Test]
+    public void When_Item_Not_Found_Get_Returns_Default_Instead_Of_Exception()
+    {
+        // normal access with [] throws exception
+        Assert.Throws<KeyNotFoundException>(() =>
+        {
+            int value = dictionary["Luca"];
+        });
+
+        // extension returns null
+        Assert.That(dictionary.Get("Luca"), Is.EqualTo(default(int)));
+    }
+
+    [Test]
+    public void When_Key_Not_Found_Set_Add_It_Instead_Of_Exception()
+    {
+        // TIL that normal access with assignment actually adds the item!
+
+        // normal access with [] throws exception
+        //Assert.Throws<KeyNotFoundException>(() =>
+        //{
+        //    // oh no it doesnt! It works fine :D
+        //    dictionary["Luca"] = 37;
+        //});
+
+        //// extension is simple
+        //dictionary.Set("Luca", 37);
+        //Assert.That(dictionary.Get("Luca"), Is.EqualTo(37));
+    }
+}
---
+++ UsefulExtensions/UsefulExtensions/DictionaryExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaoloCattaneo.UsefulExtensions;
+
+public static class DictionaryExtensions
+{
+    public static V? Get<K, V>(this Dictionary<K, V> dictionary, K key) where K : notnull
+    {
+        if(dictionary == null)
+        {
+            return default;
+        }
+
+        if(!dictionary.TryGetValue(key, out var found))
+        {
+            return default;
+        }
+
+        return found;
+    }
+
+    public static void Set<K, V>(this Dictionary<K, V> dictionary, K key, V value) where K : notnull
+    {
+        if (dictionary == null)
+        {
+            throw new ArgumentNullException(nameof(dictionary));
+        }
+
+        if (dictionary.ContainsKey(key))
+        {
+            dictionary[key] = value;
+            return;
+        }
+
+        dictionary.Add(key, value);
+    }
+
+    public static void Set<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, KeyValuePair<TKey, TValue> keyValuePair) where TKey : notnull
+    {
+        dictionary.Set(keyValuePair.Key, keyValuePair.Value);
+    }
+}
